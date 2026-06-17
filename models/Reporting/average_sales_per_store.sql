97745546cd3786053e65a6a3f89753dbb7691e34
Raymond Ogundu Owhondah <chelseaestevenicolle234@gmail.com>
2026-06-17 18:13:07 +0000

    removed unwanted folder, add test in the reporting model and modified the average sales table

--- models/Reporting/average_sales_per_store.sql
+++ models/Reporting/average_sales_per_store.sql
@@ -4,9 +4,11 @@ materialized='table') }}
 
 
 select 
+s.store_id,
 s.store_name,
 sum(fct.price) as total_sales
 from {{ ref('fact_order') }} fct 
 inner join {{ ref('dim_store') }} s 
     on s.store_id = fct.store_id
-group by 1
+group by all
+order by 1
